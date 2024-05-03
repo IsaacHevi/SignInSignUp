e5184c1be3c0cd215159203712b2343504788aca
IsaacHevi <142832860+IsaacHevi@users.noreply.github.com>
2024-05-03 22:43:35 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SignIn />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
